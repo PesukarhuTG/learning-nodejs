a9880c549df2dfeabe183f1ee1507efc298d1bab
PesukarhuTG <ganzhinovat@yandex.ru>
2023-08-27 15:13:10 +0300

    feat: add task 02

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+const { getFullUserData } = require('mpackage');
+
+const userData = {
+  name: 'максим лескин',
+  dateBirth: '10.03.1987',
+  purpose: 'карьерный рост'
+}
+
+console.log(getFullUserData(userData));
---
+++ index.mjs
@@ -0,0 +1,9 @@
+import { getFullUserData } from 'mpackage';
+
+const userData = {
+  name: 'максим лескин',
+  dateBirth: '10.03.1987',
+  purpose: 'карьерный рост'
+}
+
+console.log(getFullUserData(userData));
