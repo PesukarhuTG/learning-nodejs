7c61c9688905753eafcc0138f6c3b1770297cab5
PesukarhuTG <ganzhinovat@yandex.ru>
2023-08-29 00:06:34 +0300

    refactor: task 04 - 2

--- index-2.js
+++ index-2.js
@@ -5,17 +5,10 @@ class Message extends EventEmitter {
     super();
     this.username = '';
     this.message = '';
-  }
-
-  // расширяем метод emit для вывода в консоль
-  emit(name, ...args) {
-    super.emit(name, ...args);
-    console.log('logger', name, ...args);
+    this.init();
   }
 
   init() {
-    //3.2. Функция  receiveMessage подписывется на событие 'getMessage',
-    //которое пользователь вызывает при отправке сообщения (п.2)
     this.on('getMessage', this.receiveMessage);
 
     // вне задания: для себя протестить работу prependListener
@@ -25,29 +18,20 @@ class Message extends EventEmitter {
     });
   }
 
-  // 1. Функция для отправки сообщений пользователями
   sendMessage(username, message) {
     this.username = username;
     this.message = message;
 
-    // 2. после отравки вызывать событие с инфо о сообщении (сработает logger)
     this.emit('getMessage', { username, message });
   }
 
-  // 3.1. Функция для получения сообщения
   receiveMessage() {
-    //3.3. ...и выводит информацию о сообщении в консоль.
-    console.log(`Полученное сообщение: ${this.message}`);
+    console.log(`Текст сообщения: ${this.message}`);
   }
 }
 
 const message = new Message();
 
-// 4. Инициализация функции получения сообщений
-message.init();
-
-// 5. Напишите несколько сообщения от пользователя,
-//и удостоверьтесь, что сообщения успешно выводятся в консоль
 message.sendMessage('Tatiana', 'Hello Node.js!');
 message.sendMessage('John', 'Nice to see your code');
 message.sendMessage('Alex', 'Looking forward to the next topic');
